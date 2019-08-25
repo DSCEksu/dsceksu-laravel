287c0fd1c52689c1ae6410fb4a45bf19d5b7b479
adilimudassir <mudassiradili4u@gmail.com>
2019-08-25 23:19:29 +0100

    refactor nav and footer

---
+++ resources/views/frontend/includes/nav.blade.php
@@ -0,0 +1,25 @@
+<section id="technologies" class="section-spacer bg-very__gray">
+    <div class="container">
+      <header class="section-header text-center">
+        <h2 class="section-title">Technologies we're excited about</h2>
+        <p class="section-subtitle">Opportunities to learn & access deep technical content.</p>
+        <br>
+      </header>
+      <div class="row align-items-center">
+        <div class="col-sm-6">
+          <div class="feature-list-image">
+            <img src="{{ asset('images/frontend/technologies/android.png') }}" class="img-fluid" alt="Official android logo" width="100%" height="50%">
+          </div>
+        </div>
+        <div class="col-sm-5 ml-auto">
+          <div class="feature-list-wrapper">
+            <div class="content-header">
+              <h2 class="content-title">Android Development</h2>
+              <p>Every year Google developers release exciting new updates to the world's most popular operating system. We always have sessions to keep you updated and mastering the latest trends in modern Android development.</p>
+            </div>
+            <a href="https://codelabs.developers.google.com/?cat=Android" class="hero-button" target="_blank" rel="noreferrer">Codelabs &nbsp; &nbsp;<i class="fas fa-external-link-alt"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
