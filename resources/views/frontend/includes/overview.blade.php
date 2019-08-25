943003631ef2da60ad976a85e7cff4a697de6a72
adilimudassir <mudassiradili4u@gmail.com>
2019-08-25 23:18:12 +0100

    refactor overview page section

---
+++ resources/views/frontend/includes/overview.blade.php
@@ -0,0 +1,34 @@
+<section id="overview" class="section-spacer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-12">
+          <div class="feature-card">
+            <div class="feature-card__body">
+              <i class="fas fa-rocket ghost-blue"></i>
+              <h4 class="feature-title">Concept of DSC</h4>
+              <p>The DSC program is a grassroots channel through which Google provides development skills, mobile and web development skills for students, towards employability.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-12">
+          <div class="feature-card">
+            <div class="feature-card__body">
+              <i class="fas fa-lightbulb ghost-green"></i>
+              <h4 class="feature-title">Why DSC?</h4>
+              <p>For students to learn development skills, solve problems through technology and inspire them to be world class developers and changemakers.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-12">
+          <div class="feature-card m-0">
+            <div class="feature-card__body">
+              <i class="fas fa-users ghost-red"></i>
+              <h4 class="feature-title">Target audience</h4>
+              <p>DSC activities are targeted at University students and any others including faculty members who want to learn development skills &amp; work to solve real-life problems.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
